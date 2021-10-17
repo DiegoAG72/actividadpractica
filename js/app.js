09510a796b446415d31483c300be174d29a0a06c
Diego Giannuzzi <diegogiannuzzi@yahoo.com>
2021-10-17 18:20:47 -0300

    validate?

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 
+limpiaValue = (v) => {
+    document.querySelector(v).value = "";
+}
 
+
+
+const input = document.querySelector('input');
 var nombre = document.querySelector("#nombre").value;
 var apellido = document.querySelector("#apellido").value;
 var domicilio = document.querySelector("#domicilio").value;
@@ -7,14 +13,25 @@ var localidad = document.querySelector("#localidad").value;
 var dni = document.querySelector("#dni").value;
 if (dni.length < 7 || dni.length > 8) {
     alert("Ingrese un valor de DNI válido");
-    document.querySelector("#dni").value = "";
+    document.addEventListener("input", limpiaValue("#dni"));
 }
 var nacim = document.querySelector("#nacim").value;
 var telef = document.querySelector("#telef").value;
 if ((!Number.isInteger(parseInt(telef)))) {
     alert("Ingrese un teléfono válido");
-    document.querySelector("#telef").value = "";
+    document.addEventListener("input", limpiaValue("#telef"));
 }
 var mail = document.querySelector("#mail").value;
 
 document.write(`${nombre} ${apellido} ${domicilio} ${localidad} ${dni} ${nacim} ${telef} ${mail}`)
+
+
+/*
+const input = document.querySelector('input');
+const log = document.getElementById('valores');
+
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+  log.textContent = e.srcElement.value;
+}*/
