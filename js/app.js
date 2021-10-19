a1a68a68de7d4130bbdd5e575cade70c17264544
Diego Giannuzzi <diegogiannuzzi@yahoo.com>
2021-10-19 19:00:56 -0300

    ok

--- js/app.js
+++ js/app.js
@@ -14,4 +14,25 @@ var telef = document.querySelector("#telef");
 var mail = document.querySelector("#mail");
 
 
+ function validacion() {
+    if (dni.value.length < 7 || dni.value.length > 8) {
+        // Si no se cumple la condicion...
+        alert('Ingrese un DNI válido');
+        return false;
+    }/*
+    else if (condicion que debe cumplir el segundo campo del formulario) {
+        // Si no se cumple la condicion...
+        alert('[ERROR] El campo debe tener un valor de...');
+        return false;
+    }
+    ...
+    else if (condicion que debe cumplir el último campo del formulario) {
+        // Si no se cumple la condicion...
+        alert('[ERROR] El campo debe tener un valor de...');
+        return false;
+    }*/
 
+    // Si el script ha llegado a este punto, todas las condiciones
+    // se han cumplido, por lo que se devuelve el valor true
+    return true;
+}
