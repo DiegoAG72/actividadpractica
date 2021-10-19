f6ed1073b78dcb8865894eb1375d0cb953b7ebb9
Diego Giannuzzi <diegogiannuzzi@yahoo.com>
2021-10-19 16:37:59 -0300

    ok

--- js/app.js
+++ js/app.js
@@ -1,37 +1,17 @@
 
-limpiaValue = (v) => {
+var limpiaValue = (v) => {
     document.querySelector(v).value = "";
 }
 
-
-
 const input = document.querySelector('input');
-var nombre = document.querySelector("#nombre").value;
-var apellido = document.querySelector("#apellido").value;
-var domicilio = document.querySelector("#domicilio").value;
-var localidad = document.querySelector("#localidad").value;
-var dni = document.querySelector("#dni").value;
-if (dni.length < 7 || dni.length > 8) {
-    alert("Ingrese un valor de DNI válido");
-    document.addEventListener("input", limpiaValue("#dni"));
-}
-var nacim = document.querySelector("#nacim").value;
-var telef = document.querySelector("#telef").value;
-if ((!Number.isInteger(parseInt(telef)))) {
-    alert("Ingrese un teléfono válido");
-    document.addEventListener("input", limpiaValue("#telef"));
-}
-var mail = document.querySelector("#mail").value;
-
-document.write(`${nombre} ${apellido} ${domicilio} ${localidad} ${dni} ${nacim} ${telef} ${mail}`)
+var nombre = document.querySelector("#nombre");
+var apellido = document.querySelector("#apellido");
+var domicilio = document.querySelector("#domicilio");
+var localidad = document.querySelector("#localidad");
+var dni = document.querySelector("#dni");
+var nacim = document.querySelector("#nacim");
+var telef = document.querySelector("#telef");
+var mail = document.querySelector("#mail");
 
 
-/*
-const input = document.querySelector('input');
-const log = document.getElementById('valores');
 
-input.addEventListener('input', updateValue);
-
-function updateValue(e) {
-  log.textContent = e.srcElement.value;
-}*/
