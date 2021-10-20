a61dd60b193ef19d8b4834e634dbba6cde69711b
Diego Giannuzzi <diegogiannuzzi@yahoo.com>
2021-10-20 13:32:35 -0300

    valida

--- js/app.js
+++ js/app.js
@@ -11,4 +11,22 @@ var nacim = document.querySelector("#nacim");
 var telef = document.querySelector("#telef");
 var mail = document.querySelector("#mail");
 
+var limpiaValue = (v) => {
+    document.querySelector(v).value = "";
+}
 
+function validacion() {
+    if (dni.value.length < 7 || dni.value.length > 8) {
+        alert('Ingrese un DNI válido');
+        limpiaValue("#dni");
+        return false;
+    } else {
+        if (isNaN(telef.value) || telef.value.length < 6) {
+            alert("Ingrese un número de teléfono válido");
+            limpiaValue("#telef");
+        }
+
+
+        return true;
+    }
+}
