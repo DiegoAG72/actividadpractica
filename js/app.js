8cd79bc8b527680e906f0cea149041ede7105bb5
Diego Giannuzzi <diegogiannuzzi@yahoo.com>
2021-10-21 13:41:37 -0300

    ok

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 
-
-
 const input = document.querySelector('input');
 var nombre = document.querySelector("#nombre");
 var apellido = document.querySelector("#apellido");
@@ -11,11 +9,17 @@ var nacim = document.querySelector("#nacim");
 var telef = document.querySelector("#telef");
 var mail = document.querySelector("#mail");
 
+var cName = document.querySelector("#cName");
+var cApell = document.querySelector("#cApell");
+var cMail = document.querySelector("#cMail");
+var cTexto = document.querySelector("#cTexto");
+
+
 var limpiaValue = (v) => {
     document.querySelector(v).value = "";
 }
 
-function validacion() {
+function validacionReg() {
     if (dni.value.length < 7 || dni.value.length > 8) {
         alert('Ingrese un DNI válido');
         limpiaValue("#dni");
@@ -25,8 +29,22 @@ function validacion() {
             alert("Ingrese un número de teléfono válido");
             limpiaValue("#telef");
         }
-
-
         return true;
     }
+}
+
+function validacionContact() {
+    if (!isNaN(cName.value)) {
+        alert(`Ingrese un nombre válido`);
+        limpiaValue("#cName");
+        return false;
+    } else {
+        if (!isNaN(cApell.value)) {
+            alert(`Ingrese un apellido válido`);
+            limpiaValue("#cApell");
+            return false;
+    }
+    
+    return true;
+}
 }
